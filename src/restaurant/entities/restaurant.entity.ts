0fc3cc3edeef2f925d3c87e49fa2dd143be5e293
mathed1as <matheus.vex@gmail.com>
2022-08-16 18:46:01 -0300

    feat: add RestaurantSchema in restaurant module

--- src/restaurant/entities/restaurant.entity.ts
+++ src/restaurant/entities/restaurant.entity.ts
@@ -1,1 +1,21 @@
-export class Restaurant {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type RestaurantDocument = Restaurant & Document;
+
+@Schema()
+export class Restaurant {
+  @Prop()
+  name: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  png_link: string;
+
+  @Prop()
+  opening_hours: string;
+}
+
+export const RestaurantSchema = SchemaFactory.createForClass(Restaurant);
