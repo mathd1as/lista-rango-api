b5c7143d2e7f3e0313c8a21625ad5b38b18c6570
mathed1as <matheus.vex@gmail.com>
2022-08-19 11:46:42 -0300

    refactor: alter to local mongo db database url

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,7 @@ import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://mathd1as:teste@cluster1.eca8i.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
     UsersModule,
     RestaurantModule,
     ProductsModule,
