e1af0ec98390ef2f85527f7a07d035a4419465a7
mathed1as <matheus.vex@gmail.com>
2022-08-20 14:22:32 -0300

    feat: add class validator in create restaurant dto

--- src/restaurant/dto/create-restaurant.dto.ts
+++ src/restaurant/dto/create-restaurant.dto.ts
@@ -1,6 +1,14 @@
+import { IsNotEmpty, IsEmail } from 'class-validator';
 export class CreateRestaurantDto {
+  @IsNotEmpty()
   png_link: string;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   address: string;
+
+  @IsNotEmpty()
   opening_hours: string;
 }
